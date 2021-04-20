54a0638c6c98fafb6c77c2dd2789bdef059812b2
Ariana Peck <apeck@slac.stanford.edu>
2021-04-19 20:51:59 -0700

    Adding function to compute mean alignment error with symmetry

--- cmtip/alignment/errors.py
+++ cmtip/alignment/errors.py
@@ -21,3 +21,19 @@ def alignment_error(q1,q2):
     cos_theta = np.around(cos_theta, decimals=5) # ensure within arccos domain
     error = np.arccos(cos_theta)
     return np.rad2deg(error)
+
+
+def mean_error(errors, symmetry=2):
+    """
+    Compute the mean alignment error, taking symmetry into account.
+    
+    :param errors: array of orientation errors
+    :param symmetry: particle's symmetry fold
+    :return mean_error: mean orientation error
+    """
+    interval=360/symmetry
+    symm_vals = np.arange(0,180+interval,interval)
+    symm_vals = np.tile(symm_vals, errors.shape[0]).reshape(errors.shape[0],len(symm_vals))
+    
+    symm_diffs = np.abs(errors - symm_vals.T)
+    return np.mean(np.min(symm_diffs, axis=0))
