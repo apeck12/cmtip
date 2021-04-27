a3e31ca43bf1323ae793e7e762a961f4157f699f
Ariana Peck <apeck@slac.stanford.edu>
2021-04-26 19:59:19 -0700

    changes to enable increasing M during reconstruction

--- cmtip/phasing/phase_mpi.py
+++ cmtip/phasing/phase_mpi.py
@@ -6,6 +6,7 @@ try:
 except ImportError:
     print("MPI seems unavailable")
 
+
 def phase_mpi(comm, generation, ac, support_=None, rho_=None, n_iterations=10, nER=50, nHIO=25):
     """
     Wrapper for phasing using MPI. Phasing is performed on the first rank only by 
@@ -33,3 +34,35 @@ def phase_mpi(comm, generation, ac, support_=None, rho_=None, n_iterations=10, n
     
     comm.Bcast(ac_phased, root=0)
     return ac_phased, support_, rho_
+
+
+def resize_mpi(comm, support_, rho_, M_next, r_extent_orig, r_extent_next):
+    """
+    Wrapper for resizing the support and density volumes.
+
+    :param comm: MPI intracommunicator instance
+    :param support_: support estimate
+    :param rho_: density estimate
+    :param M_next: cubic length of density volume of next iteration
+    :param r_extent_orig: reciprocal space extent of current iteration
+    :param r_extent_next: reciprocal space extent of next iteration
+    :return support_: resized support estimate
+    :return rho_: resized density estimate
+    """
+    if comm.rank == 0:
+        # shift DC component to center
+        support = np.fft.ifftshift(support_)
+        rho = np.fft.ifftshift(rho_)
+        
+        # resize volume
+        support = phase_base.resize_volume(support, M_next, r_extent_orig, r_extent_next)
+        rho = phase_base.resize_volume(rho, M_next, r_extent_orig, r_extent_next)  
+        
+        # shift DC component back to corner
+        support_ = np.fft.fftshift(support)
+        rho_ = np.fft.fftshift(rho)
+
+    else:
+        support_, rho_ = None, None
+    
+    return support_, rho_
